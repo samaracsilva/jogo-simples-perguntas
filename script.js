616913935003065eb265921f4f630fb8767bba31
Samara Silva <68922817+samaracsilva@users.noreply.github.com>
2021-06-08 00:09:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// PROBLEMA
+// Crie um quiz com as seguintes perguntas:
+
+// Pergunta: De quem é a famosa frase “Penso, logo existo”?
+// Resposta: Descartes
+
+// Quanto tempo a luz do Sol demora para chegar à Terra?
+// 8 minutos
+
+// A resposta deve ser um alert() contendo true ou false
+
+var pergunta1 = prompt('De quem é a famosa frase: Penso, logo existo ?');
+pergunta1 = pergunta1.toLowerCase();
+var resposta1 = 'descartes';
+alert(pergunta1 === resposta1);
+
+var pergunta2 = prompt('Quanto tempo a Luz do Sol demora para chegar à Terra: ?');
+pergunta2 = pergunta2.toLowerCase();
+var resposta2 = '8 minutos';
+alert(pergunta2 === resposta2);
+
+
